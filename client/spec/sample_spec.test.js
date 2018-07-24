853b67a4b4cd8bd8b80450a5c810702b456a12fb
Khie Biggs <khiebigzy@gmail.com>
2018-07-24 14:30:15 +0100

    Set up testing for travis CLI Travis previously wasn't testing correctly, as it ran all of the tests in the express folder, but did not run the tests in the client folder; This meant that travis might have approved and automatically merged a commit when there were still errors in it

--- client/spec/sample_spec.test.js
+++ client/spec/sample_spec.test.js
@@ -3,8 +3,3 @@ describe("nothing", function(){
         expect(5).toEqual(5);
     });
 });
-describe("something", function(){
-    it("changes basic math laws", function(){
-        expect(4).toEqual(5);
-    });
-});
