a9c9ed5e2c71c2d53be977bb77eb35ffbb0fe4c4
Khie Biggs <khiebigzy@gmail.com>
2018-07-24 10:51:51 +0100

    Set up express

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/hello', (req, res) =>{
+    res.json("Hello World");
+    console.log("Sent hello world");
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Server listening on ${port}`);
